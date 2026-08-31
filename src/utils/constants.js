90e589ebd74530b90d21601eba1b6dde650b84bd
Iasmim Horrana <horranaiasmim@gmail.com>
2026-08-31 15:34:47 -0300

    att-cache

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ export const END_DATE = '2027-02-14';
 export const TOTAL_DIAS = 183;
 
 export const CACHE_KEY = 'diario_cache_v1';
-export const CACHE_TTL_MS = 12 * 60 * 60 * 1000;
+export const CACHE_TTL_MS = 60 * 60 * 1000;
 
 export const READ_TRACKING_KEY = 'diario_lidos_v1';
 export const DEBUG_TODAY_SESSION_KEY = 'diario_debug_today';
